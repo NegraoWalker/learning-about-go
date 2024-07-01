daa175d2191681b6d5aa49e616c72200fef7b112
walkerNegrão <walker.esteves.negrao@gmail.com>
2024-07-01 17:27:15 -0300

    Criação do repositório

---
+++ go-testes-wn/values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("String!")
+	fmt.Println("Operação aritmética com inteiros: 10 + 5 = ", 10+5)
+	fmt.Println("Operação aritmética com decimais: 1.567 + 5.567 = ", 1.567+5.567)
+	fmt.Println("Operação aritmética com decimais: 1.567 - 5.567 = ", 1.567-5.567)
+	fmt.Println("Operação aritmética com decimais: 1.567 / 5.567 = ", 1.567/5.567)
+	fmt.Println("Operação aritmética com decimais: 1.567 * 5.567 = ", 1.567*5.567)
+
+}
